99b82a43563596d74ad0c1b4484a4019b022b665
hjl2011 <hjl2011@yeah.net>
2023-06-03 10:42:59 +0800

    v0.1.2-alpha.3
    - 取消对英语的支持
      ~ 排版问题
    + 可显示历史最高 Rating
      ~ 默认不显示
    + 1min 内只能运行一次

--- README.md
+++ README.md
@@ -6,17 +6,22 @@
 ![](https://img.shields.io/github/package-json/v/hjl2011/luogu-user-elo-rating-card?style=flat-square)
 ![](https://img.shields.io/github/license/hjl2011/luogu-user-elo-rating-card?style=flat-square)
 
-由于技术原因，本脚本生成的图像不会自动更新。
-**也请不要在短时间内多次运行本脚本，任何后果均由使用者自负，开发者不承担任何责任。**
+由于技术原因，本脚本生成的图像并不会自动更新。
+
+**警告：请不要在短时间内多次运行本脚本，任何后果均由使用者自负，开发者不承担任何责任。**
+
+**警告：请不要在短时间内多次运行本脚本，任何后果均由使用者自负，开发者不承担任何责任。**
+
+**警告：请不要在短时间内多次运行本脚本，任何后果均由使用者自负，开发者不承担任何责任。**
 
 1. 将 `config-example.json` 重命名为 `config.json`。
-2. 将 `config.uid` 改为您的洛谷 uid。
-3. 将 `config.lang` 改为您使用的语言，目前仅支持：
-   - `zh-CN`（简体中文）
-   - `zh-TW`（繁体中文）
-   - `en-US`（英文）
-4. 设置 `config.path` 为您要生成的 SVG 文件的路径。
-5. 运行 `npm install`。
-6. 运行 `npm start`。
-7. 得到 SVG 文件。
-8. 更新 SVG 文件请运行 `npm start`。
+2. 修改 `config.json`。
+   - `uid`：洛谷 uid。
+   - `lang`：语言。
+   - `path`：SVG 文件路径。
+   - `enable-show-history-maxvalue`：是否显示历史最高 Rating。
+3. 设置 `config.path` 为您要生成的 SVG 文件的路径。
+4. 运行 `npm install`。
+5. 运行 `npm start`。
+6. 得到 SVG 文件。
+7. 更新 SVG 文件请运行 `npm start`。
--- app.js
+++ app.js
@@ -5,29 +5,34 @@ const process = require('process');
 config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
 lang = JSON.parse(fs.readFileSync('lang.json', 'utf-8'));
 
-var svg_file = fs.createWriteStream(config.path);
+if (new Date(config.time).valueOf() + 60000 > new Date().valueOf()) {
+    console.log("Please wait 1min.");
+    process.exit(0);
+}
 
 if (config.uid <= 0) {
     console.error("config.uid <= 0");
     process.exit(1);
 }
 
-if (!['zh-CN', 'zh-TW', 'en-US'].includes(config.lang)) {
+if (!['zh-CN', 'zh-TW'].includes(config.lang)) {
     console.error("config.lang is incorrect.");
     process.exit(1);
 }
 
+var svg_file = fs.createWriteStream(config.path);
+
 function Date_Format(date) {
     var str = date.getFullYear();
-    if(date.getMonth() + 1 < 10) str += `-0${date.getMonth() + 1}`;
+    if (date.getMonth() + 1 < 10) str += `-0${date.getMonth() + 1}`;
     else str += `-${date.getMonth() + 1}`;
-    if(date.getDate() < 10) str += `-0${date.getDate()}`;
+    if (date.getDate() < 10) str += `-0${date.getDate()}`;
     else str += `-${date.getDate()}`;
-    if(date.getHours() < 10) str += ` 0${date.getHours()}`;
+    if (date.getHours() < 10) str += ` 0${date.getHours()}`;
     else str += ` ${date.getHours()}`;
-    if(date.getMinutes() < 10) str += `:0${date.getMinutes()}`;
+    if (date.getMinutes() < 10) str += `:0${date.getMinutes()}`;
     else str += `:${date.getMinutes()}`;
-    if(date.getSeconds() < 10) str += `:0${date.getSeconds()}`;
+    if (date.getSeconds() < 10) str += `:0${date.getSeconds()}`;
     else str += `:${date.getSeconds()}`;
     return str;
 }
@@ -36,7 +41,7 @@ function Date_Format(date) {
     var user = await fetch(`https://www.luogu.com.cn/user/${config.uid}`, {
         headers: [["x-luogu-type", "content-only"]]
     }).then(response => response.json()).then(res => res.currentData.user);
-    svg_file.write('<svg width="350" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg" style="cursor:default;user-select:none;">');
+    svg_file.write(`<svg width="350" height="${190 + (config['enable-show-history-maxvalue'] ? 30 : 0)}" version="1.1" xmlns="http://www.w3.org/2000/svg" style="cursor:default;user-select:none;">`);
     svg_file.write('<style>rect{fill:#fff;stroke-width:.75px;stroke:#a5a5a5;}.user{fill:#5eb95e;}.rating{fill:#bc10f0;}.contest{fill:#109af0;}.time{fill:#ea7a13;}.item{fill:#3c5dd8;}</style>');
     svg_file.write('<rect width="100%" height="100%" rx="6" ry="6" />');
     svg_file.write(`<text x="15" y="30" style="font-size:18px;">${lang[config.lang].title}</text>`);
@@ -48,6 +53,23 @@ function Date_Format(date) {
     svg_file.write(`<text x="100" y="120" class="contest">${user.elo.contest.name}</text>`);
     svg_file.write(`<text x="15" y="150" class="item">${lang[config.lang].time}</text>`);
     svg_file.write(`<text x="100" y="150" class="time">${Date_Format(new Date(user.elo.time * 1000))}</text>`);
-    svg_file.write(`<text x="${config.lang != 'en-US' ? 100 : 50}" y="180" style="fill: #888;font-size: 14px;">${lang[config.lang].card_generation_time}${Date_Format(new Date())}</text>`);
-    svg_file.write(`</svg>`);
+    if (config['enable-show-history-maxvalue']) {
+        var api = await fetch(`https://www.luogu.com.cn/api/rating/elo?user=${config.uid}`, {
+            headers: [["x-luogu-type", "content-only"]]
+        }).then(response => response.json()).then(res => res.records.result);
+        maxr = -0x7f7f7f7f;
+        for (var i = 0; i < api.length; i++) {
+            if (api[i].rating > maxr) maxr = api[i].rating;
+        }
+        svg_file.write(`<text x="15" y="180" class="item">${lang[config.lang].historymax}</text>`);
+        svg_file.write(`<text x="100" y="180" class="rating">${maxr}</text>`);
+    }
+    svg_file.write(`<text x="100" y="${175 + (config['enable-show-history-maxvalue'] ? 30 : 0)}" style="fill: #888;font-size: 14px;">${lang[config.lang].card_generation_time}${Date_Format(new Date())}</text>`);
+    svg_file.write('</svg>');
+    config.time = Date_Format(new Date());
+    fs.writeFile('config.json', JSON.stringify(config, null, 4), function (err) {
+        if (!err) return;
+        console.error(err);
+        process.exit(1);
+    });
 })();

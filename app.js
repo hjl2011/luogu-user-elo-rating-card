091f8526a2234a8589bf82ad8ab7f4f643d28e21
hjl2011 <100132650+hjl2011@users.noreply.github.com>
2023-06-04 13:56:50 +0800

    v0.1.2-alpha.4
    # 修正 SVG 宽度

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function Date_Format(date) {
     var user = await fetch(`https://www.luogu.com.cn/user/${config.uid}`, {
         headers: [["x-luogu-type", "content-only"]]
     }).then(response => response.json()).then(res => res.currentData.user);
-    svg_file.write(`<svg width="350" height="${190 + (config['enable-show-history-maxvalue'] ? 30 : 0)}" version="1.1" xmlns="http://www.w3.org/2000/svg" style="cursor:default;user-select:none;">`);
+    svg_file.write(`<svg width="${Math.max(350, 13 * user.elo.contest.name.length + 40)}" height="${190 + (config['enable-show-history-maxvalue'] ? 30 : 0)}" version="1.1" xmlns="http://www.w3.org/2000/svg" style="cursor:default;user-select:none;">`);
     svg_file.write('<style>rect{fill:#fff;stroke-width:.75px;stroke:#a5a5a5;}.user{fill:#5eb95e;}.rating{fill:#bc10f0;}.contest{fill:#109af0;}.time{fill:#ea7a13;}.item{fill:#3c5dd8;}</style>');
     svg_file.write('<rect width="100%" height="100%" rx="6" ry="6" />');
     svg_file.write(`<text x="15" y="30" style="font-size:18px;">${lang[config.lang].title}</text>`);
@@ -64,7 +64,7 @@ function Date_Format(date) {
         svg_file.write(`<text x="15" y="180" class="item">${lang[config.lang].historymax}</text>`);
         svg_file.write(`<text x="100" y="180" class="rating">${maxr}</text>`);
     }
-    svg_file.write(`<text x="100" y="${175 + (config['enable-show-history-maxvalue'] ? 30 : 0)}" style="fill: #888;font-size: 14px;">${lang[config.lang].card_generation_time}${Date_Format(new Date())}</text>`);
+    svg_file.write(`<text x="${100 + ((13 * user.elo.contest.name.length + 40 - 350) > 0 ? (13 * user.elo.contest.name.length + 40 - 350) : 0)}" y="${175 + (config['enable-show-history-maxvalue'] ? 30 : 0)}" style="fill: #888;font-size: 14px;">${lang[config.lang].card_generation_time}${Date_Format(new Date())}</text>`);
     svg_file.write('</svg>');
     config.time = Date_Format(new Date());
     fs.writeFile('config.json', JSON.stringify(config, null, 4), function (err) {

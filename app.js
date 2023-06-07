8f3b7c41bd14be6e512e5cfa522550145164dd66
hjl2011 <hjl2011@yeah.net>
2023-06-07 22:10:36 +0800

    v0.1.2-alpha.7
    + 判断一下用户没有参加过 Rated 比赛的情况

--- app.js
+++ app.js
@@ -41,31 +41,35 @@ function Date_Format(date) {
     var user = await fetch(`https://www.luogu.com.cn/user/${config.uid}`, {
         headers: [["x-luogu-type", "content-only"]]
     }).then(response => response.json()).then(res => res.currentData.user);
-    svg_file.write(`<svg width="${Math.max(350, 13 * user.elo.contest.name.length + 40)}" height="${190 + (config['enable-show-history-maxvalue'] ? 30 : 0)}" version="1.1" xmlns="http://www.w3.org/2000/svg" style="cursor:default;user-select:none;">`);
-    if(!config.darkmode) svg_file.write('<style>rect{fill:#fff;stroke-width:.75px;stroke:#a5a5a5;}.title{font-size:18px;fill:#333;}.user{fill:#5eb95e;}.rating{fill:#bc10f0;}.contest{fill:#109af0;}.time{fill:#ea7a13;}.item{fill:#3c5dd8;}</style>');
-    else svg_file.write('<style>rect{fill:#666;stroke-width:.75px;stroke:#a5a5a5;}.title{font-size:18px;fill:#eee;}.user{fill:#5eb95e;}.rating{fill:#ffe65b;}.contest{fill:#109af0;}.time{fill:#26ea13;}.item{fill:#58ead2;}</style>');
+    svg_file.write(`<svg width="${null == user.elo ? 300 : Math.max(350, 13 * user.elo.contest.name.length + 40)}" height="${null == user.elo ? 80 : (190 + (config['enable-show-history-maxvalue'] ? 30 : 0))}" version="1.1" xmlns="http://www.w3.org/2000/svg" style="cursor:default;user-select:none;">`);
+    if(!config.darkmode) svg_file.write('<style>rect{fill:#fff;stroke-width:.75px;stroke:#a5a5a5;}.title{font-size:18px;fill:#333;}.user{fill:#5eb95e;}.rating{fill:#bc10f0;}.contest{fill:#109af0;}.time{fill:#ea7a13;}.item{fill:#3c5dd8;}.error{fill:#f01111;}</style>');
+    else svg_file.write('<style>rect{fill:#666;stroke-width:.75px;stroke:#a5a5a5;}.title{font-size:18px;fill:#eee;}.user{fill:#5eb95e;}.rating{fill:#ffe65b;}.contest{fill:#109af0;}.time{fill:#26ea13;}.item{fill:#58ead2;}.error{fill:#f01111;}</style>');
     svg_file.write('<rect width="100%" height="100%" rx="6" ry="6" />');
     svg_file.write(`<text x="15" y="30" class="title">${lang[config.lang].title}</text>`);
-    svg_file.write(`<text x="15" y="60" class="item">${lang[config.lang].user}</text>`);
-    svg_file.write(`<text x="100" y="60" class="user">${user.name} (${config.uid})</text>`);
-    svg_file.write(`<text x="15" y="90" class="item">${lang[config.lang].rating}</text>`);
-    svg_file.write(`<text x="100" y="90" class="rating">${user.elo.rating}</text>`);
-    svg_file.write(`<text x="15" y="120" class="item">${lang[config.lang].contest}</text>`);
-    svg_file.write(`<text x="100" y="120" class="contest">${user.elo.contest.name}</text>`);
-    svg_file.write(`<text x="15" y="150" class="item">${lang[config.lang].time}</text>`);
-    svg_file.write(`<text x="100" y="150" class="time">${Date_Format(new Date(user.elo.time * 1000))}</text>`);
-    if (config['enable-show-history-maxvalue']) {
-        var api = await fetch(`https://www.luogu.com.cn/api/rating/elo?user=${config.uid}`, {
-            headers: [["x-luogu-type", "content-only"]]
-        }).then(response => response.json()).then(res => res.records.result);
-        maxr = -0x7f7f7f7f;
-        for (var i = 0; i < api.length; i++) {
-            if (api[i].rating > maxr) maxr = api[i].rating;
+    if(null == user.elo) {
+        svg_file.write(`<text x="15" y="60" class="error">${lang[config.lang].errorinfo}</text>`);
+    } else {
+        svg_file.write(`<text x="15" y="60" class="item">${lang[config.lang].user}</text>`);
+        svg_file.write(`<text x="100" y="60" class="user">${user.name} (${config.uid})</text>`);
+        svg_file.write(`<text x="15" y="90" class="item">${lang[config.lang].rating}</text>`);
+        svg_file.write(`<text x="100" y="90" class="rating">${user.elo.rating}</text>`);
+        svg_file.write(`<text x="15" y="120" class="item">${lang[config.lang].contest}</text>`);
+        svg_file.write(`<text x="100" y="120" class="contest">${user.elo.contest.name}</text>`);
+        svg_file.write(`<text x="15" y="150" class="item">${lang[config.lang].time}</text>`);
+        svg_file.write(`<text x="100" y="150" class="time">${Date_Format(new Date(user.elo.time * 1000))}</text>`);
+        if (config['enable-show-history-maxvalue']) {
+            var api = await fetch(`https://www.luogu.com.cn/api/rating/elo?user=${config.uid}`, {
+                headers: [["x-luogu-type", "content-only"]]
+            }).then(response => response.json()).then(res => res.records.result);
+            maxr = -0x7f7f7f7f;
+            for (var i = 0; i < api.length; i++) {
+                if (api[i].rating > maxr) maxr = api[i].rating;
+            }
+            svg_file.write(`<text x="15" y="180" class="item">${lang[config.lang].historymax}</text>`);
+            svg_file.write(`<text x="100" y="180" class="rating">${maxr}</text>`);
         }
-        svg_file.write(`<text x="15" y="180" class="item">${lang[config.lang].historymax}</text>`);
-        svg_file.write(`<text x="100" y="180" class="rating">${maxr}</text>`);
+        svg_file.write(`<text x="${100 + ((13 * user.elo.contest.name.length + 40 - 350) > 0 ? (13 * user.elo.contest.name.length + 40 - 350) : 0)}" y="${175 + (config['enable-show-history-maxvalue'] ? 30 : 0)}" style="fill: ${config.darkmode ? '#eee' : '#999'};font-size: 14px;">${lang[config.lang].card_generation_time}${Date_Format(new Date())}</text>`);
     }
-    svg_file.write(`<text x="${100 + ((13 * user.elo.contest.name.length + 40 - 350) > 0 ? (13 * user.elo.contest.name.length + 40 - 350) : 0)}" y="${175 + (config['enable-show-history-maxvalue'] ? 30 : 0)}" style="fill: ${config.darkmode ? '#eee' : '#999'};font-size: 14px;">${lang[config.lang].card_generation_time}${Date_Format(new Date())}</text>`);
     svg_file.write('</svg>');
     config.time = Date_Format(new Date());
     fs.writeFile('config.json', JSON.stringify(config, null, 4), function (err) {
